ee2c4bf1e8a1e69ae47e6e93b67f2887da4cfd16
Mark Estrada <mark.estrada.dev>
2022-08-03 11:55:23 -0500

    correcting routes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -5,6 +6,7 @@ import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
+dotenv.config();
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
@@ -14,8 +16,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL =
-  'mongodb+srv://markestrada:markestrada1234@cluster0.me4gjcy.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
